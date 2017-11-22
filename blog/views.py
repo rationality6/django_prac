11becded9d4e9f95a7006568b28c5bf8f0f66ded
rationality6 <rationality6@gmail.com>
2017-11-22 13:57:44 +0900

    complete django girls extensions facture0

--- blog/views.py
+++ blog/views.py
@@ -23,7 +23,7 @@ def post_new(request):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
+            # post.published_date = timezone.now()
             post.save()
             return redirect('blog:post_detail', pk=post.pk)
     else:
@@ -33,7 +33,7 @@ def post_new(request):
 
 def post_draft_list(request):
     posts = Post.objects.filter(
-        published_date__isnull=True).order_by('created_date')
+        published_date__isnull=True).order_by('-created_date')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
 
@@ -50,3 +50,15 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect("blog:post_detail", pk=pk)
+
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect("blog:post_list")

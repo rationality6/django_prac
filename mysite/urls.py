8d67698089f99c278da34004eb4f6782bbb26619
rationality6 <rationality6@gmail.com>
2017-11-22 18:59:30 +0900

    added a model for make comments

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from django.contrib.auth import views
+
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^accounts/login/$', views.login, name='login'),
+    url(r'^accounts/logout/$', views.logout, name='logout', kwargs={'next_page':'/'}),
     url(r'^', include('main.urls')),
     url(r'^blog/', include('blog.urls')),
 ]

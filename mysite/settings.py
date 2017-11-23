5b37ac8665be92195bd4fb6b8082283edee8fd8d
rationality6 <rationality6@gmail.com>
2017-11-23 10:32:59 +0900

    test

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,8 +11,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = '13kl3#fqi-nm&&-1wu#q1k5-@%1ibmm#a9ya-ueep8m)ww*=b_'
 
-# DEBUG = True
-DEBUG = False
+DEBUG = True
+# DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com','localhost']
 
